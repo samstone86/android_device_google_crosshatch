32493142712b0b3a8b8a9debbcd824207ba3f162
Nolen Johnson <johnsonnolen@gmail.com>
2018-11-27 20:42:52 +0000

    crosshatch: Compile kernel in-line
    * Define need for DTBO image and remove prebuilt logic.
    * No need for prebuilt kernel/kernel modules logic as we'll never use it.
    * We use Clang built kernel.
    
    Change-Id: I9483a46c294cbc1edf594cf5ec77556d36e20735

--- BoardConfig-omni.mk
+++ BoardConfig-omni.mk
@@ -17,4 +17,15 @@
 # SELinux
 include vendor/omni/sepolicy/sepolicy.mk
 
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := b1c1_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
+
 -include vendor/google/crosshatch/BoardConfigVendor.mk
