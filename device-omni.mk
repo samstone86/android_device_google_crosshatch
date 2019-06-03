4d652ac5343a18503fe9a58943490e245301a8cd
Philipp Vogel <philippwolf86.pw@gmail.com>
2019-06-03 19:47:28 +0200

    crosshatch: Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device-omni.mk
+++ device-omni.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
