ad4a4ed0b0a029fc2019cef70dd6f746d7d82058
Philipp Vogel <philippwolf86.pw@gmail.com>
2019-06-03 17:19:26 +0200

    crosshatch: initial OmniROM bringup
    also add sepolicy fixes

--- device-omni.mk
+++ device-omni.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright 2019 The OmniROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crosshatch.mk \
-    $(LOCAL_DIR)/aosp_blueline.mk \
-    $(LOCAL_DIR)/omni_crosshatch.mk \
-    $(LOCAL_DIR)/omni_blueline.mk \
+# Camera
+PRODUCT_PACKAGES += \
+    SnapdragonCamera2
