4cf44c6bf1dc1629cc936fb71eb2b256b4baf916
Philipp Vogel <philippwolf86.pw@gmail.com>
2019-06-03 19:43:57 +0200

    crosshatch: Compile kernel in-line
    * Define need for DTBO image and remove prebuilt logic.
    * No need for prebuilt kernel/kernel modules logic as we'll never use it.
    * We use Clang built kernel.
    
    Change-Id: I9483a46c294cbc1edf594cf5ec77556d36e20735

--- BoardConfig-omni.mk
+++ BoardConfig-omni.mk
@@ -14,4 +14,12 @@
 # limitations under the License.
 #
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := crosshatch_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/crosshatch
+
 -include vendor/google/crosshatch/BoardConfigVendor.mk
